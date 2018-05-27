7f077ca6d717402fb973b5483256c2f669c06e94
Ingmar Wittkau <iwittkau@users.noreply.github.com>
2018-05-27 20:01:37 +0200

    Fixed errors from CI

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -12,35 +12,43 @@ func TestClient_SendSTUNRequest_Parallel(t *testing.T) {
 	}
 
 	// simple channel fo go routine start signaling
-	started := make(chan struct{}, 0)
+	started := make(chan struct{})
+	finished := make(chan struct{})
+	var err1 error
+	var resp1 interface{}
 
 	// stun1.l.google.com:19302, more at https://gist.github.com/zziuni/3741933#file-stuns-L5
 	go func() {
 		close(started)
-		resp, err := c.SendSTUNRequest(net.IPv4(74, 125, 143, 127), 19302)
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Log(resp)
+		resp1, err1 = c.SendSTUNRequest(net.IPv4(74, 125, 143, 127), 19302)
+		close(finished)
 	}()
 
 	// block until go routine is started to make two almost parallel requests
-	select {
-	case <-started:
-	}
-	resp, err := c.SendSTUNRequest(net.IPv4(74, 125, 143, 127), 19302)
-	if err != nil {
+
+	<-started
+
+	resp2, err2 := c.SendSTUNRequest(net.IPv4(74, 125, 143, 127), 19302)
+	if err2 != nil {
 		t.Fatal(err)
+	} else {
+		t.Log(resp2)
 	}
-	t.Log(resp)
+
+	<-finished
+	if err1 != nil {
+		t.Fatal(err)
+	} else {
+		t.Log(resp1)
+	}
 
 }
 
 func TestNewClient_Errors(t *testing.T) {
 
-	_, err := NewClient("255.255.255.255:65535")
+	_, err := NewClient("255.255.255.256:65535")
 	if err == nil {
-		t.Fatal("listening on 255.255.255.255:65535 should fail")
+		t.Fatal("listening on 255.255.255.256:65535 should fail")
 	}
 
 	// Unable to perform this test atm because there is no timeout and the test may run infinitely
