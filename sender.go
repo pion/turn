5c66135cd4b52d65d24305de2b414f131660409a
Igor German <igor.german@synesis.ru>
2019-07-11 13:58:51 +0300

    Added tests for SOFTWARE attribute
    Added tests to confirm that SOFTWARE is added to
    requests/responses

--- sender.go
+++ sender.go
@@ -9,10 +9,8 @@ import (
 // Sender is responsible for building a message and sending it to the given addr.
 type Sender func(conn net.PacketConn, addr net.Addr, attrs ...stun.Setter) error
 
-// DefaultSender is a sender default implementation.
-var DefaultSender Sender = buildAndSend
-
-func buildAndSend(conn net.PacketConn, dst net.Addr, attrs ...stun.Setter) error {
+// DefaultBuildAndSend is a sender default implementation.
+func DefaultBuildAndSend(conn net.PacketConn, dst net.Addr, attrs ...stun.Setter) error {
 	msg, err := stun.Build(attrs...)
 	if err != nil {
 		return err
