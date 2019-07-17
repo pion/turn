e6c745d4df3760de73f89a121a7142556d845b0d
Yutaka Takeda <yt0916@gmail.com>
2019-07-16 20:05:54 -0700

    Remove unportable code from internal/ipnet
    Upgraded pion/transport to v0.8.6
    Relates to pion/webrtc#740

--- internal/ipnet/controlmessage.go
+++
@@ -1,8 +0,0 @@
-package ipnet
-
-import "net"
-
-// A ControlMessage represents per packet basis IP-level socket options.
-type ControlMessage struct {
-	Dst net.IP // destination address, receiving only
-}
