8a9e6c87ae50fe259ec82c5f43146b1f8f387fe3
David Zhao <david@davidzhao.com>
2022-02-26 21:44:37 -0800

    Set read deadline to handle abandoned allocations
    Giving it 30s deadline for allocation requests.

--- internal/allocation/allocation.go
+++ internal/allocation/allocation.go
@@ -13,6 +13,10 @@ import (
 	"github.com/pion/turn/v2/internal/proto"
 )
 
+const (
+	allocationDeadline = 30 * time.Second
+)
+
 type allocationResponse struct {
 	transactionID [stun.TransactionIDSize]byte
 	responseAttrs []stun.Setter
@@ -245,6 +249,7 @@ func (a *Allocation) packetHandler(m *Manager) {
 	buffer := make([]byte, rtpMTU)
 
 	for {
+		_ = a.RelaySocket.SetReadDeadline(time.Now().Add(allocationDeadline))
 		n, srcAddr, err := a.RelaySocket.ReadFrom(buffer)
 		if err != nil {
 			m.DeleteAllocation(a.fiveTuple)
