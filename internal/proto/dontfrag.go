d7aa620721b4bea83ca3946e5fa31fbcd4e38388
Steffen Vogel <post@steffenvogel.de>
2023-02-04 11:13:48 +0100

    Make DontFragmentAttr implement stun.Getter
    As its needed for fuzzing it.

--- internal/proto/dontfrag.go
+++ internal/proto/dontfrag.go
@@ -1,13 +1,29 @@
 package proto
 
-import "github.com/pion/stun"
+import (
+	"github.com/pion/stun"
+)
 
 // DontFragmentAttr represents DONT-FRAGMENT attribute.
 type DontFragmentAttr struct{}
 
+const dontFragmentSize = 0
+
 // AddTo adds DONT-FRAGMENT attribute to message.
 func (DontFragmentAttr) AddTo(m *stun.Message) error {
 	m.Add(stun.AttrDontFragment, nil)
+	return nil
+}
+
+// GetFrom decodes DONT-FRAGMENT from message.
+func (d *DontFragmentAttr) GetFrom(m *stun.Message) error {
+	v, err := m.Get(stun.AttrDontFragment)
+	if err != nil {
+		return err
+	}
+	if err = stun.CheckSize(stun.AttrDontFragment, len(v), dontFragmentSize); err != nil {
+		return err
+	}
 	return nil
 }
 
