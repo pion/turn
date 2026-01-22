aa561001ea4a1c2037a9aa6acf8af8f1120ed22d
Pion <59523206+pionbot@users.noreply.github.com>
2026-01-22 08:46:18 +0000

    Update CI configs to v0.11.37
    Update lint scripts and CI configs.

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package turn contains the public API for pion/turn, a toolkit for building TURN clients and servers
